d290ee10a4ba5fb82065d561c1577e32a976125b
Aleksandr <aleks.poleshchuk@gmail.com>
2020-07-30 23:20:20 +0300

    cors fix

--- src/axios-config.js
+++ src/axios-config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create
 ({
-     baseURL: 'https://2019ncov.asia/api'
+     baseURL: "https://cors-anywhere.herokuapp.com/https://2019ncov.asia/api"
  });
 
 export default instance;
